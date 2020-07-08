8307ce722afa05e368a596733721891de3bcdcab
Gabriel Rios <gabrielfalcaorios@gmail.com>
2020-07-08 10:48:25 -0300

    Add indent-guides

--- config.el
+++ config.el
@@ -113,8 +113,23 @@
 ;;; :lang org
 (setq org-directory "~/OneDrive/org/"
       org-ellipsis " ▼ ")
+(setq org-journal-dir "~/OneDrive/journal")
+
 (after! org
   (add-to-list 'org-modules 'org-habit t))
 
 
 (add-to-list 'auto-mode-alist '("\\.rhtml\\'" . web-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
