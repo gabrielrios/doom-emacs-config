ee3656b3169d7f07fd7f32a9a8d7a87d7439093b
Gabriel Rios <gabrielfalcaorios@gmail.com>
2021-02-23 11:26:58 -0300

    enable fill-column

--- packages.el
+++ packages.el
@@ -12,8 +12,7 @@
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
+;(package! another-package :recipe (:host github :repo "username/repo"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
